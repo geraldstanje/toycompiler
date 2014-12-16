31d4a371a0ee5e061d5fc92e6e6efe44d93fe11e
Gerald Stanje <Gerald.Stanje@gmail.com>
2014-12-16 23:19:58 +0100

    changed architecture

---
+++ codegen.go
@@ -0,0 +1,129 @@
+package dsl
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type CodeGenerator interface {
+	CompTopScope(Node) error
+}
+
+type AsmCodeGenerator struct {
+	err    string
+	ast    Node
+	indent int
+	writer *bufio.Writer
+}
+
+func NewAsmCodeGenerator() *AsmCodeGenerator {
+	return &AsmCodeGenerator{}
+}
+
+func (c *AsmCodeGenerator) EmitLine(line string) {
+	c.writer.WriteString(strings.Repeat("\t", c.indent))
+	c.writer.WriteString(line)
+	c.writer.WriteString("\n")
+}
+
+func (c *AsmCodeGenerator) Indent() {
+	c.indent += 1
+}
+
+func (c *AsmCodeGenerator) Deindent() {
+	c.indent -= 1
+
+	if c.indent < 0 {
+		c.indent = 0
+	}
+}
+
+func (c *AsmCodeGenerator) compNode(node Node) {
+	if node == nil {
+		return
+	}
+
+	switch n := node.(type) {
+	case *ProgramNode:
+		c.compNode(n.Front())
+		c.compNode(n.Next())
+
+	case *TokenNode:
+		c.EmitLine(fmt.Sprintf("PUSH %s", n.Token))
+
+	case *AssignNode:
+		left := n.Front()
+		right := n.Next()
+
+		if tn, ok := right.(*TokenNode); ok {
+			c.EmitLine(fmt.Sprintf("PUSH %s", tn.Token))
+		} else if _, ok := right.(*OpNode); ok {
+			c.compNode(right)
+		}
+		if tn, ok := left.(*TokenNode); ok {
+			c.EmitLine(fmt.Sprintf("SET %s", tn.Token))
+		}
+
+	case *OpNode:
+		left := n.Front()
+		right := n.Next()
+
+		c.compNode(left)
+		c.compNode(right)
+
+		if n.Operator == "+" {
+			c.EmitLine("ADD")
+		} else if n.Operator == "-" {
+			c.EmitLine("SUB")
+		} else if n.Operator == "/" {
+			c.EmitLine("DIV")
+		} else if n.Operator == "*" {
+			c.EmitLine("MUL")
+		}
+
+	case *PrintNode:
+		left := n.Front()
+
+		c.compNode(left)
+		c.EmitLine("PRINT")
+
+	case *WhileNode:
+		left := n.Front()
+		right := n.Next()
+		blockNb = blockNb + 1
+
+		c.EmitLine(fmt.Sprintf("JMP cond%d", blockNb))
+		c.EmitLine(fmt.Sprintf("body%d%s", blockNb, ":"))
+
+		c.Indent()
+		c.compNode(right)
+		c.Deindent()
+
+		c.EmitLine(fmt.Sprintf("cond%d%s", blockNb, ":"))
+
+		c.Indent()
+		c.compNode(left)
+		c.EmitLine(fmt.Sprintf("JNZ body%d", blockNb))
+		c.Deindent()
+	}
+
+	return
+}
+
+func (c *AsmCodeGenerator) CompTopScope(ast Node) error {
+	c.ast = ast
+
+	file, err := os.Create("generated.txt")
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+	c.writer = bufio.NewWriter(file)
+
+	c.compNode(c.ast)
+
+	c.writer.Flush()
+	return nil
+}
--- comp.go
+++ comp.go
@@ -1,17 +1,14 @@
 package dsl
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 type Compiler struct {
-	err    string
-	ast    Node
-	indent int
-	writer *bufio.Writer
+	err        string
+	ast        Node
+	asmCodegen CodeGenerator
 }
 
 func NewCompiler() *Compiler {
@@ -48,106 +45,8 @@ func (c *Compiler) PlotAst(filename string) error {
 	return err
 }
 
-func (c *Compiler) EmitLine(line string) {
-	c.writer.WriteString(strings.Repeat("\t", c.indent))
-	c.writer.WriteString(line)
-	c.writer.WriteString("\n")
-}
-
-func (c *Compiler) Indent() {
-	c.indent += 1
-}
-
-func (c *Compiler) Deindent() {
-	c.indent -= 1
-
-	if c.indent < 0 {
-		c.indent = 0
-	}
-}
-
-func (c *Compiler) compNode(node Node) {
-	if node == nil {
-		return
-	}
-
-	switch n := node.(type) {
-	case *ProgramNode:
-		c.compNode(n.Front())
-		c.compNode(n.Next())
-
-	case *TokenNode:
-		c.EmitLine(fmt.Sprintf("PUSH %s", n.Token))
-
-	case *AssignNode:
-		left := n.Front()
-		right := n.Next()
-
-		if tn, ok := right.(*TokenNode); ok {
-			c.EmitLine(fmt.Sprintf("PUSH %s", tn.Token))
-		} else if _, ok := right.(*OpNode); ok {
-			c.compNode(right)
-		}
-		if tn, ok := left.(*TokenNode); ok {
-			c.EmitLine(fmt.Sprintf("SET %s", tn.Token))
-		}
-
-	case *OpNode:
-		left := n.Front()
-		right := n.Next()
-
-		c.compNode(left)
-		c.compNode(right)
-
-		if n.Operator == "+" {
-			c.EmitLine("ADD")
-		} else if n.Operator == "-" {
-			c.EmitLine("SUB")
-		} else if n.Operator == "/" {
-			c.EmitLine("DIV")
-		} else if n.Operator == "*" {
-			c.EmitLine("MUL")
-		}
-
-	case *PrintNode:
-		left := n.Front()
-
-		c.compNode(left)
-		c.EmitLine("PRINT")
-
-	case *WhileNode:
-		left := n.Front()
-		right := n.Next()
-		blockNb = blockNb + 1
-
-		c.EmitLine(fmt.Sprintf("JMP cond%d", blockNb))
-		c.EmitLine(fmt.Sprintf("body%d%s", blockNb, ":"))
-
-		c.Indent()
-		c.compNode(right)
-		c.Deindent()
-
-		c.EmitLine(fmt.Sprintf("cond%d%s", blockNb, ":"))
-
-		c.Indent()
-		c.compNode(left)
-		c.EmitLine(fmt.Sprintf("JNZ body%d", blockNb))
-		c.Deindent()
-	}
-
-	return
-}
-
 func (c *Compiler) CompTopScope() error {
-	file, err := os.Create("generated.txt")
-	defer file.Close()
-	if err != nil {
-		return err
-	}
-	c.writer = bufio.NewWriter(file)
-
-	c.compNode(c.ast)
-
-	c.writer.Flush()
-	return nil
+	c.asmCodegen = NewAsmCodeGenerator()
+	err := c.asmCodegen.CompTopScope(c.ast)
+	return err
 }
